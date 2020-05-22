7f5661788ab1f183a57585f1f9c0e14fc5ccc934
mdccalex <mcphersona7@ww.catholic.edu.au>
2020-05-22 15:08:46 +1000

    Create rubric_export.js

---
+++ rubric_export.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Speedgrader Word Count
+// @namespace    https://github.com/mdccalex
+// @description  Export a Canvas rubric in the format used to import.
+// @include      https://*.instructure.com/accounts/*/rubrics/*
+// @include      https://*.instructure.com/courses/*/rubrics/*
+// @require      https://code.jquery.com/jquery-3.5.1.min.js
+// @grant        none
+// @run-at       document-idle
+// @version      1.0.0
+// ==/UserScript==
